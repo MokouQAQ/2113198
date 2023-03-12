3cc13bd69fd5f0664f7f609e6509155aa6b5f96a
MokouQAQ <2113198@mail.nankai.edu.cn>
2023-03-12 21:56:13 +0800

    666

---
+++ 平常做题用.cpp
@@ -0,0 +1,72 @@
+﻿#include<string>
+#include<cstring>
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<iomanip>
+#include<deque>
+#include<queue>
+#include<fstream>
+#include<algorithm>
+#include<sstream>
+#include<set>
+#include<map>
+#include<numeric>
+#include<list>
+#include<cmath>
+#include <windows.h>
+using namespace std;
+const int N = 10000; // matrix size
+double a[N], b[N][N], sum[N] = {0};
+
+void inits(int n) // generate a N∗N matrix
+{
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			b[i][j] = i + j;
+
+}
+
+int main()
+{
+	fill(a, a + N, 1);
+	long long head, tail, freq; // timers
+	inits(N);
+	// similar to CLOCKS_PER_SEC
+	QueryPerformanceFrequency((LARGE_INTEGER *) & freq);
+	// start time
+	QueryPerformanceCounter((LARGE_INTEGER *) & head);
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			sum[j] += a[i] * b[i][j];
+	//unroll优化算法
+	/* for (int i = 0; i < N; i++)
+	{
+		int j = 0;
+		for (; j + 3 < N; j += 4)
+		{
+			sum[j] += a[i] * b[i][j];
+			sum[j + 1] += a[i] * b[i][j + 1];
+			sum[j + 2] += a[i] * b[i][j + 2];
+			sum[j + 3] += a[i] * b[i][j + 3];
+		}
+	}
+
+}
+逐列访问平凡算法
+for (int i = 0; i < N; i++) {
+	for (int j = 0; j < N; j++)
+		sum[i] += b[j][i] * a[j];
+}*/
+
+//cache优化算法
+for (int i = 0; i < N; i++)
+	for (int j = 0; j < N; j++)
+		sum[j] += a[i] * b[i][j];
+	
+	// end time
+	QueryPerformanceCounter((LARGE_INTEGER *) & tail);
+	cout << "Col: " << (tail -head) * 1000.0 / freq
+		<< "ms" << endl;
+}
---
+++ 并行第第一次二题.cpp
@@ -0,0 +1,71 @@
+﻿#include<string>
+#include<cstring>
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<iomanip>
+#include<deque>
+#include<queue>
+#include<fstream>
+#include<algorithm>
+#include<sstream>
+#include<set>
+#include<map>
+#include<numeric>
+#include<list>
+#include<cmath>
+#include <windows.h>
+using namespace std;
+const int N = 100; // matrix size
+double a[N] ;
+long long sum,sum1,sum2,sum3,sum4;
+void inits(int n) // generate a N∗N matrix
+{
+	
+
+}
+//方法三：递归算法
+/*void adds(int n) {
+	if (n <= 1)
+		return;
+	else
+	{
+		for (int i = 0; i < n / 2; i++)
+			a[i] += a[n - i - 1];
+
+		adds(n / 2);
+	}
+
+}*/
+
+int main()
+{
+	sum = 0;
+	fill(a, a + N, 1);
+	long long head, tail, freq; // timers
+	inits(N);
+	// similar to CLOCKS_PER_SEC
+	QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+	// start time
+	QueryPerformanceCounter((LARGE_INTEGER*)&head);
+
+	//方法一:平凡算法累加
+	/*for (int i = 0; i < N; i++)sum += a[i];*/
+
+
+	//方法二：四路链式累加
+	/*for (int i = 1; i <= N - 3; i += 4)
+	{
+		int sum1 = a[i];
+		int sum2 = a[i + 1];
+		int sum3 = a[i + 2];
+		int sum4 = a[i + 3];
+	}
+	sum = sum1 + sum2 + sum3 + sum4;
+	*/
+
+	// end time
+	QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+	cout << "Col: " << (tail - head) * 1000.0 / freq
+		<< "ms" << endl;
+}
